449dc473fad7692969a90165e004f0144b2734ef
j.morsch <jannis.morsch@web.de>
2020-12-09 09:38:44 +0100

    add a BETA tag to identify areas for future work

--- plugins/+FeatureExtraction/alaFeature.m
+++ plugins/+FeatureExtraction/alaFeature.m
@@ -18,6 +18,8 @@
 % You should have received a copy of the GNU Affero General Public License
 % along with this program.  If not, see <http://www.gnu.org/licenses/>. 
 
+% !!BETA!!: Visualization of features not working properly, computation
+% works as expected
 function info = alaFeature()
     info.type = DataProcessingBlockTypes.FeatureExtraction;
     info.caption = 'Adaptive Linear Approximation';
