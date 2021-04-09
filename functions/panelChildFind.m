0a3726c8da2ae1e5060aaffb2b8c16c152daa3d7
j.morsch <jannis.morsch@web.de>
2021-04-09 19:32:50 +0200

    add functions to conveniently find and set the currently visible child element of a uipanel

---
+++ functions/panelChildFind.m
@@ -0,0 +1,33 @@
+% This file is part of DAVE, a MATLAB toolbox for data evaluation.
+% Copyright (C) 2018-2019 Saarland University, Author: Manuel Bastuck
+% Website/Contact: www.lmt.uni-saarland.de, info@lmt.uni-saarland.de
+% 
+% The author thanks Tobias Baur, Tizian Schneider, and Jannis Morsch
+% for their contributions.
+% 
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Affero General Public License as published
+% by the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU Affero General Public License for more details.
+% 
+% You should have received a copy of the GNU Affero General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+
+function index = panelChildFind(panel)
+%PANELCHILDFIND Find the currently selected panel child, return its index
+%   Loop through all children, return the index of the first visible child
+
+index = 1;
+for j = 1:size(panel.Children,1)
+    if panel.Children(j).Visible == 1
+        index = j;
+        break
+    end
+end
+end
+
