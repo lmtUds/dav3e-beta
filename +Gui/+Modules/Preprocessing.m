fcc26f222b69e0644d73fba606697daf2a29b8e9
j.morsch <jannis.morsch@web.de>
2021-07-02 14:04:11 +0200

    experiment with disabling the tables during dragging

--- +Gui/+Modules/Preprocessing.m
+++ +Gui/+Modules/Preprocessing.m
@@ -1102,7 +1102,7 @@ classdef Preprocessing < Gui.Modules.GuiModule
             % the corresponding cycle line bold
 %             obj.cyclePointTable.setCallbacksActive(false);
 %             objRow = obj.cyclePointTable.getRowObjectRow(gObj);
-%             obj.cyclePointTable.jTable.getSelectionModel().setSelectionInterval(objRow-1,objRow-1);
+%             obj.cyclePointTable.Enable = 'off';
             idx = ismember(obj.cyclePoints,gObj);
             obj.hLines.current.raw.cycle(idx).LineWidth = 2;
             obj.hLines.current.pp.cycle(idx).LineWidth = 2;
@@ -1125,6 +1125,7 @@ classdef Preprocessing < Gui.Modules.GuiModule
                 obj.hLines.compare.raw.cycle(idx).LineWidth = 1;
                 obj.hLines.compare.pp.cycle(idx).LineWidth = 1;
             end
+%             obj.cyclePointTable.Enable = 'on';
 %             objRow = obj.cyclePointTable.getRowObjectRow(gObj);
 %             obj.cyclePointTable.jTable.getSelectionModel().setSelectionInterval(objRow-1,objRow-1);
 % %             obj.cyclePointTable.setRowHeader();
@@ -1138,7 +1139,7 @@ classdef Preprocessing < Gui.Modules.GuiModule
             % the corresponding quasistatic line bold
 %             obj.indexPointTable.setCallbacksActive(false);
 %             objRow = obj.indexPointTable.getRowObjectRow(gObj);
-%             obj.indexPointTable.jTable.getSelectionModel().setSelectionInterval(objRow-1,objRow-1);
+%             obj.indexPointTable.Enable = 'off';
             idx = ismember(obj.indexPoints,gObj);
             obj.hLines.current.raw.quasistatic(idx).LineWidth = 2;
             obj.hLines.current.pp.quasistatic(idx).LineWidth = 2;
@@ -1161,6 +1162,7 @@ classdef Preprocessing < Gui.Modules.GuiModule
                 obj.hLines.compare.raw.quasistatic(idx).LineWidth = 1;
                 obj.hLines.compare.pp.quasistatic(idx).LineWidth = 1;
             end
+%             obj.indexPointTable.Enable = 'on';
 %             objRow = obj.indexPointTable.getRowObjectRow(gObj);
 %             obj.indexPointTable.jTable.getSelectionModel().setSelectionInterval(objRow-1,objRow-1);
 % %             obj.indexPointTable.setRowHeader();
