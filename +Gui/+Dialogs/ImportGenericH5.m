5af3c9b51ef53f57e33872b725bd447307ff5cbb
j.morsch <jannis.morsch@web.de>
2022-05-20 15:56:23 +0200

    ImportH5: add new function style inside old construct

--- +Gui/+Dialogs/ImportGenericH5.m
+++ +Gui/+Dialogs/ImportGenericH5.m
@@ -32,6 +32,7 @@ classdef ImportGenericH5 < handle
     
     methods
         function obj = ImportGenericH5(main,datasetNames)
+            obj.ImportGenericH5New(datasetNames);
             obj.main = main;
             obj.f = figure('Name','H5 Clusters','WindowStyle','modal');
             layout = uiextras.VBox('Parent',obj.f);
@@ -55,6 +56,70 @@ classdef ImportGenericH5 < handle
             obj.sliderDragged();
             
             layout.Sizes = [30,-1,30];
+        end
+        
+        function [GroupMap, Exit] = ImportGenericH5New(obj,datasetNames)            
+            GroupMap = containers.Map();
+            Exit = 0;
+            fig = uifigure('Name','H5 Clusters','WindowStyle','modal',...
+                'Visible','off');
+            grid = uigridlayout(fig,[3 1],'RowHeight',{'1x','4x',22});
+                        
+            obj.datasetNames = datasetNames;
+            [atoms,nMin,nMax] = PathAtoms(datasetNames);
+            
+            slider = uislider(grid,'Limits',[nMin nMax],'Value',nMax,...
+                'MajorTickLabels',{});
+            slider.Layout.Row = 1;
+            
+            listBox = uilistbox(grid);
+            listBox.Layout.Row = 2;
+            slider.ValueChangedFcn = @(src,event)...
+                Dragged(src,event,listBox,atoms);
+            
+            okButton = uibutton(grid,'Text','Ok',...
+                'ButtonPushedFcn',@(src,event)...
+                    Ok(src,event,slider,listBox,datasetNames,atoms));
+            okButton.Layout.Row = 3;
+            
+            Dragged(slider,[],listBox,atoms);
+            
+            fig.Visible = 'on';
+            uiwait(fig)
+            function [atoms,nMin,nMax] = PathAtoms(datasetNames)
+                atoms = regexp(datasetNames, '/', 'split');
+                nums = cellfun(@numel,atoms);
+                nMin = 1;
+                nMax = min(nums);
+            end
+            function [roots,n] = Roots(atoms,n)
+                roots = cell(1,numel(atoms));
+                for i = 1:numel(atoms)
+                    a = atoms{i};
+                    roots{i} = strjoin(a(1:n),'/');
+                end
+                [roots,~,idx] = unique(string(roots));
+                n = histcounts(idx, 1:numel(roots));
+            end
+            function Dragged(src,event,list,atoms)
+                [roots,n] = Roots(atoms,src.Value);
+                s = cell(1,numel(n));
+                for i = 1:numel(n)
+                    s{i} = sprintf('%s (%d)',roots{i},n(i));
+                end
+                list.Items = s;
+                list.Value = s;
+            end
+            function Ok(src,event,slider,listBox,datasetNames,atoms)                
+                roots = Roots(atoms,slider.Value);
+                roots = roots(ismember(listBox.Items,listBox.Value));
+                for i = 1:numel(roots)
+                    idx = logical(cell2mat(strfind(datasetNames,roots{i})));
+                    GroupMap(roots{i}) = datasetNames(idx);
+                end
+                Exit = 1;
+                close(fig);
+            end
         end
 
         function [roots,n] = getRoots(obj,n)
