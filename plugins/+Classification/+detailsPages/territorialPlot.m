327d3bb284ec2ed4994d48e15290567110fc8f6a
j.morsch <jannis.morsch@web.de>
2022-06-10 14:05:38 +0200

    adapt territorial plot to new ui style

--- plugins/+Classification/+detailsPages/territorialPlot.m
+++ plugins/+Classification/+detailsPages/territorialPlot.m
@@ -18,41 +18,17 @@
 % You should have received a copy of the GNU Affero General Public License
 % along with this program.  If not, see <http://www.gnu.org/licenses/>. 
 
-function [panel,updateFun] = territorialPlot(parent,project,dataprocessingblock)
-    [panel,elements] = makeGui(parent);
-    populateGui(elements,project,dataprocessingblock);
-    updateFun = @()populateGui(elements,project,dataprocessingblock);
+function updateFun = territorialPlot(parent,project,dataprocessingblock)
+    populateGui(parent,project,dataprocessingblock);
+    updateFun = @()populateGui(parent,project,dataprocessingblock);
 end
 
-function [panel,elements] = makeGui(parent)
-    panel = uipanel(parent);
-    layout = uiextras.VBox('Parent',panel);
-    panel2 = uipanel(layout,'BorderType','none');
-    hAx = axes(panel2); title('');
-    xlabel('DF1'); ylabel('DF2');
-    box on,
-    set(gca,'LooseInset',get(gca,'TightInset')) % https://undocumentedmatlab.com/blog/axes-looseinset-property
-    elements.hAx = hAx;
-end
-
-function populateGui(elements,project,dataprocessingblock)
-    cla(elements.hAx,'reset');
+function populateGui(parent,project,dataprocessingblock)
     dataParam = dataprocessingblock.parameters.getByCaption('inputData');
     if isempty(dataParam)
         return
     end
-%     cumEnergy = dataprocessingblock.parameters.getByCaption('cumEnergy').getValue();
-    groupingCaption = project.currentModel.fullModelData.groupingCaption;
-    groupingObj = project.getGroupingByCaption(groupingCaption);
-    groupingColors = groupingObj.getColors();
-    trainGrouping = deStar(project.currentModel.fullModelData.getSelectedGrouping('training',groupingCaption));
     trainData = dataParam.getValue().training;
-    if isfield(dataParam.getValue(),'testing')
-        testData = dataParam.getValue().testing;
-    else
-        testData = [];
-    end
-    testGrouping = deStar(project.currentModel.fullModelData.getSelectedGrouping('testing',groupingCaption));
     dims = 1:size(trainData,2);
     if numel(dims) ~= 2
         warning('Territorial plot only available for 2D.')
@@ -62,6 +38,15 @@ function populateGui(elements,project,dataprocessingblock)
 %         warning('Showing only first two of %d dimensions.',size(data.training,2));
 %         dims = dims(1:3);
 %     end
+
+    groupingCaption = project.currentModel.fullModelData.groupingCaption;
+    groupingObj = project.getGroupingByCaption(groupingCaption);
+    groupingColors = groupingObj.getColors();
+    if isfield(dataParam.getValue(),'testing')
+        testData = dataParam.getValue().testing;
+    else
+        testData = [];
+    end
 
     mins = min([trainData;testData],[],1);
     maxs = max([trainData;testData],[],1);
@@ -77,13 +62,11 @@ function populateGui(elements,project,dataprocessingblock)
     cats = unique(deStar(p));
     p = reshape(p,res,res);
     
-    if iscategorical(p)
-        trainGrouping = categorical(trainGrouping);
-        testGrouping = categorical(testGrouping);
-    end
-    
-%     figure, hold on;
-    hold(elements.hAx,'on');
+    ax = uiaxes(parent);
+    ax.Layout.Row = 1;
+    ax.Layout.Column = 1;
+    xlabel(ax,'DF1'); ylabel(ax,'DF2');
+    hold(ax,'on');
     for i = 1:numel(cats)
         bin = p==cats(i);
         bin = padarray(bin,[1,1]);
@@ -130,7 +113,7 @@ function populateGui(elements,project,dataprocessingblock)
                     end
                     u = baseVecX(x2-1);
                     v = baseVecY(y2-1);
-                    ptch = patch(elements.hAx,u,v,'red');
+                    ptch = patch(ax,u,v,'red');
                     try
                         ptch.FaceColor = groupingColors(char(cats(i)));
                     catch
@@ -146,61 +129,16 @@ function populateGui(elements,project,dataprocessingblock)
         end
     end
     
-    h = plot(elements.hAx,trainData(:,1),trainData(:,2),'ok');
+    h = plot(ax,trainData(:,1),trainData(:,2),'ok');
     h.MarkerSize = 3;
     h.MarkerFaceColor = 'k';
     if ~isempty(testData)
-        h = plot(elements.hAx,testData(:,1),testData(:,2),'ok');
+        h = plot(ax,testData(:,1),testData(:,2),'ok');
         h.MarkerSize = 3;
         h.MarkerFaceColor = 'w';
     end
     
-    hold(elements.hAx,'off');
-    xlim(elements.hAx,[mins(1),maxs(1)]);
-    ylim(elements.hAx,[mins(2),maxs(2)]);
-end
-
-function [handles,captions] = scatterPlot(hAx,data,grouping,dims,groupingColors)
-    handles = [];
-    captions = string.empty;
-    hold(hAx,'on');
-    cats = categories(grouping);
-    for i = 1:numel(cats)
-        idx = grouping == cats{i};
-        if numel(dims) == 1
-            p = scatter(hAx,data(idx,dims),zeros(sum(idx),1),50,'filled');
-        elseif numel(dims) == 2
-            p = scatter(hAx,data(idx,dims(1)),data(idx,dims(2)),50,'filled');
-        elseif numel(dims) == 3
-            grid(hAx,'on');
-            p = scatter3(hAx,data(idx,dims(1)),data(idx,dims(2)),data(idx,dims(3)),50,'filled');
-            centroid = mean(data(idx,dims),1);
-            l = plot3(hAx,centroid(1)*[1,1],centroid(2)*[1,1],[-1000,1000],'--','Color',groupingColors(cats{i}));
-            l(2) = plot3(hAx,centroid(1)*[1,1],[-1000,1000],centroid(3)*[1,1],'--','Color',groupingColors(cats{i}));
-            l(3) = plot3(hAx,[-1000,1000],centroid(2)*[1,1],centroid(3)*[1,1],'--','Color',groupingColors(cats{i}));
-            set(l,'XLimInclude','off','YLimInclude','off','ZLimInclude','off');
-        else
-            error('Expected one, two, or three dimensions.');
-        end
-        
-        if isempty(handles)
-            handles = p;
-            captions = string(cats{i});
-        else
-            handles(end+1) = p;
-            captions(end+1) = cats{i};
-        end
-        set(p,'MarkerFaceColor',groupingColors(cats{i}),...
-              'MarkerEdgeColor',groupingColors(cats{i}) / 2);
-    end
-%     set(handles,'MarkerEdgeAlpha',0.5,'MarkerFaceAlpha',0.7,'LineWidth',0.01);
-    hold(hAx,'off');
-end
-
-function spinAxes(hAx)
-    v = get(hAx,'View');
-    for i = 0:1:360
-        set(hAx,'View',v + [i 0]);
-        pause(0.05)
-    end
+    hold(ax,'off');
+    xlim(ax,[mins(1),maxs(1)]);
+    ylim(ax,[mins(2),maxs(2)]);
 end
