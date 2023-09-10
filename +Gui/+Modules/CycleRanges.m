c08be64a822075be2d3b1ced7ccd7c1084188e9f
d_becher <daniel-e.becher@t-online.de>
2023-09-10 12:11:12 +0200

    Added separator in CycleRange Menu Dropdown

--- +Gui/+Modules/CycleRanges.m
+++ +Gui/+Modules/CycleRanges.m
@@ -50,9 +50,10 @@ classdef CycleRanges < Gui.Modules.GuiModule
                 'RowSpacing',7);
             
             moduleMenu = uimenu(mainFigure,'Label','CycleRanges');
-            uimenu(moduleMenu,'Label','import cycle ranges', getMenuCallbackName(),@(varargin)obj.onClickImport);
             uimenu(moduleMenu,'Label','export cycle ranges', getMenuCallbackName(),@(varargin)obj.onClickExport);
-            uimenu(moduleMenu,'Label','change range length (batch)', getMenuCallbackName(),@(varargin)obj.onClickChangeRangeLength);
+            uimenu(moduleMenu,'Label','import cycle ranges', getMenuCallbackName(),@(varargin)obj.onClickImport);
+            uimenu(moduleMenu,'Label','change range length (batch)','Separator','on', getMenuCallbackName(),@(varargin)obj.onClickChangeRangeLength);
+            
             uimenu(moduleMenu,'Label','make cycle ranges and grouping from selected sensor', getMenuCallbackName(),@(varargin)obj.onClickMakeCycleRangesAndGroupingFromSelectedSensor);
 
             rangeAx = uiaxes(moduleLayout);
